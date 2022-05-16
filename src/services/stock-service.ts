02b64bb6c81f9d1d88a4f2152b48e4ebcad2c524
Ruslan Kuyanets <ruslan.kuyanets@yandex.ru>
2022-05-16 21:08:54 +0300

    MG-7 add filter by name, add limit and offset for getting stocks

--- src/services/stock-service.ts
+++ src/services/stock-service.ts
@@ -1,8 +1,8 @@
 import Stock from '../models/Stocks/Stock'
 
 class StockService {
-    async getStocks() {
-        return await Stock.find()
+    async getStocks(offset: number = 1, limit: number = 20, name = '') {
+        return await Stock.find({"description": { $regex: new RegExp(name, 'ig') }}).skip(+offset).limit(+limit)
     }
 }
 
